6694d378d10def98f2d4bd0503e4fbb03c51c157
David Maack <david.maack@arcor.de>
2014-02-20 15:49:32 +0100

    added autoload.ini and autload.php

---
+++ config/autoload.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Contao Open Source CMS
+ *
+ * Copyright (c) 2005-2014 Leo Feyer
+ *
+ * @package Contaomaps_demosupplier
+ * @link    https://contao.org
+ * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ */
+
+
+/**
+ * Register the classes
+ */
+ClassLoader::addClasses(array
+(
+	'ContaoMapLayerDemo' => 'system/modules/contaomaps_demosupplier/ContaoMapLayerDemo.php',
+));
